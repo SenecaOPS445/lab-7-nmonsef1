d293b17209d1e7bf7c5b511a3e38d9103da55e42
niyayesh monsef <nmonsef@myseneca.ca>
2024-07-26 07:56:29 -0400

    completed invest1-part1

--- lab7a.py
+++ lab7a.py
@@ -20,6 +20,12 @@ def sum_times(t1, t2):
     sum.hour = t1.hour + t2.hour
     sum.minute = t1.minute + t2.minute
     sum.second = t1.second + t2.second
+    if sum.second >= 60: 
+        sum.second = sum.second - 60 
+        sum.minute = sum.minute + 1 
+    if sum.minute >= 60: 
+        sum.minute = sum.minute - 60 
+        sum.hour = sum.hour + 1 
     return sum
 
 def valid_time(t):
